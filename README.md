55361649430aa0511edf72a5e76a41a2a577ba9b
k1LoW <k1lowxb@gmail.com>
2011-01-05 11:19:52 +0900

    Add allowed settings

--- README.md
+++ README.md
@@ -13,7 +13,11 @@ Second, add the following code in app_controller.php
 
     <?php
         class AppController extends Controller {
-            var $components = array('Maintenace.Maintenance');
+            var $components = array('Maintenance.Maintenance' => array('maintenanceUrl' => array('controller' => 'public',
+                                                                                                 'action' => 'maintenance'),
+                                                                       'allowIp'        => array('127.0.0.1'), // allowed IP address when maintanance status
+                                                                       'allowed'        => array('posts' => array('index'))) // allowed action when maintanance status
+                                   );
         }
 
 ## Usage ##
--- controllers/components/maintenance.php
+++ controllers/components/maintenance.php
@@ -1,37 +1,68 @@
 <?php
-/**
- * Site Maintenance component for CakePHP.
- *
- * Copyright (c) by Shintaro Sugimoto
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @license http://www.opensource.org/licenses/mit-license.php The MIT License
- */
+  /**
+   * Site Maintenance component for CakePHP.
+   *
+   * Copyright (c) by Shintaro Sugimoto
+   *
+   * Licensed under The MIT License
+   * Redistributions of files must retain the above copyright notice.
+   *
+   * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+   */
 class MaintenanceComponent extends Object {
     var $components = array('RequestHandler');
+
+    public $maintenanceUrl;
+    public $allowed = array();
+    public $allowIp = array();
 
     public function __construct() {
         parent::__construct();
     }
 
-    public function initialize($controller, $settings = array()) {
-        $defaults = array('maintenanceUrl' => '/', 'allowIp' => '127.0.0.1');
-        $settings = Set::merge($defaults, $settings);
+    public function initialize(&$controller, $settings = array()) {
+        if (empty($settings['maintenanceUrl'])) {
+            return true;
+        }
 
-        $maintenanceUrl = Router::url($settings['maintenanceUrl'], true);
+        $this->controller = $controller;
+        $this->_set($settings);
+
+        $maintenanceUrl = Router::url($this->maintenanceUrl, true);
         $clientIp = $this->RequestHandler->getClientIP();
         $maintenanceEnable = Configure::read('Maintenance.enable');
         if ($maintenanceEnable === true) {
-            if ( Router::url('', true) != $maintenanceUrl && !in_array($clientIp, (array)$settings['allowIp'])) {
+            // maintenance status
+
+            if ( !$this->isAllowed($this->controller->params)
+                 && strstr(Router::url('', true), $controller->webroot)
+                 && Router::url('', true) != Router::url($maintenanceUrl, true)
+                 && !in_array($clientIp, (array)$this->allowIp)) {
                 $controller->redirect($maintenanceUrl);
             }
         } else {
+            // on service
+
             if (Router::url('', true) == $maintenanceUrl) {
                 $controller->redirect('/');
             }
         }
     }
 
-}
+    /**
+     * isAllowed
+     *
+     * @param $params
+     * @return
+     */
+    public function isAllowed($params){
+        if (!array_key_exists($params['controller'], $this->allowed)) {
+            return false;
+        }
+        $actions = (array) $this->allowed[$params['controller']];
+        if ($actions === array('*')) {
+            return true;
+        }
+        return (in_array($params['action'], $actions));
+    }
+  }
